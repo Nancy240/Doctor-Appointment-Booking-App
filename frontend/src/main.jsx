90edc9e34b174a9b3413a51d6f9a88b74c32d860
Nancy240 <nancydwivedi1234@gmail.com>
2025-04-08 14:36:23 +0530

    first-commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
